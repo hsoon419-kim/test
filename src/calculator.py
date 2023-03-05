270418abfc7e234b3c8a7326b77b9b45cc4ac244
hsoon419-kim <hsoon419.kim@gmail.com>
2023-03-05 15:38:26 +0900

    test

---
+++ src/calculator.py
@@ -0,0 +1,20 @@
+# calculator.py
+class Calculator(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+    @staticmethod
+    def divide(a, b):
+        return a / b
--- test_add.py
+++ test_add.py
@@ -1,12 +1,24 @@
-"""
-test file
-"""
+import pytest
 
-from main import test
+from src.calculator import Calculator
 
 
-def test_answer():
-    """
-    test func
-    """
-    assert test(3) == 5
+@pytest.fixture
+def calculator():
+    calculator = Calculator()
+    return calculator
+
+
+def test_add(calculator):
+    assert calculator.add(1, 2) == 3
+    assert calculator.add(2, 2) == 4
+
+
+def test_subtract(calculator):
+    assert calculator.subtract(5, 1) == 4
+    assert calculator.subtract(3, 2) == 1
+
+
+def test_multiply(calculator):
+    assert calculator.multiply(2, 2) == 4
+    assert calculator.multiply(5, 6) == 30
